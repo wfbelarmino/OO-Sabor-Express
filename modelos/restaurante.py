5ab200b2b39f73712518d65035deb02a9f7096fb
wagner <wfbelarmino@gmail.com>
2024-06-17 14:44:45 -0300

    01 03 - Classes

---
+++ modelos/restaurante.py
@@ -0,0 +1,11 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_pizza = Restaurante()
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(restaurantes)
