0ac1fcc9450198f753dea99cb8feacf452fa66c5
wagner <wfbelarmino@gmail.com>
2024-06-20 19:10:34 -0300

    02 03 Metodos especiais

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,10 +4,13 @@ class Restaurante:
         self.categoria = categoria
         self.ativo = False
 
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
+print(restaurante_praca)
+print(restaurante_pizza)
