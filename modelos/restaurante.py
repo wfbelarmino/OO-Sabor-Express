5f73ce004315fe6396dedd7f2ab86e202cf75b38
wagner <wfbelarmino@gmail.com>
2024-06-24 19:19:31 -0300

    04 04 Composição

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -29,3 +29,12 @@ class Restaurante:
     def receber_avaliacao(self, cliente, nota):
         avaliacao = Avaliacao(cliente, nota)
         self._avaliacao.append(avaliacao)
+        
+    @property
+    def media_avaliacoes(self): 
+        if not self._avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
+        return media
