c751440d369151a42c4ca4b332864fae8bf96f8e
wagner <wfbelarmino@gmail.com>
2024-06-24 18:33:19 -0300

    04 02 From e import

---
+++ app.py
@@ -0,0 +1,13 @@
+from modelos.restaurante import Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
+restaurante_japones = Restaurante('Japa', 'Japonesa')
+
+restaurante_mexicano.alternar_estado()
+
+def main():
+     Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -23,8 +23,3 @@ class Restaurante:
     def alternar_estado(self):
         self._ativo = not self._ativo        
 
-restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.alternar_estado()
-restaurante_pizza = Restaurante('pizza express', 'Italiana')
-
-Restaurante.listar_restaurantes()
