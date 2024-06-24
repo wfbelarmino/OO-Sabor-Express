29d81d800a6e6e1b24e9635bc338105ed65cb1a7
wagner <wfbelarmino@gmail.com>
2024-06-24 18:57:19 -0300

    04 03 Criando a classe de avaliação

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,3 +1,5 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
 
@@ -5,6 +7,7 @@ class Restaurante:
         self._nome = nome.title()
         self.categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
@@ -23,3 +26,6 @@ class Restaurante:
     def alternar_estado(self):
         self._ativo = not self._ativo        
 
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
