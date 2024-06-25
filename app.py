1bf3cff86ad7d3fa76be6bef882b1acef87c62f7
wagner <wfbelarmino@gmail.com>
2024-06-25 10:30:29 -0300

    04 05 Listando avaliações 01

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from modelos.restaurante import Restaurante
 restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_praca.receber_avaliacao('Gui', 10)
 restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Emy', 5)
+restaurante_praca.receber_avaliacao('Emy', 2)
 
 def main():
     Restaurante.listar_restaurantes()
