b8becb8e2e5b5165c41351c9ff76567981448a5b
wagner <wfbelarmino@gmail.com>
2024-06-25 10:06:53 -0300

    04 05 Listando avaliações

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -15,10 +15,10 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} |{'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
-            
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} |{restaurante.ativo}')
+    
     @property
     def ativo(self):
         return '⌧' if self._ativo else '☐'   
