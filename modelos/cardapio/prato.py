bc1d2902599995f35a07719c42291cf5c7163c7e
wagner <wfbelarmino@gmail.com>
2024-06-26 19:29:13 -0300

    03.01.04 - Herança

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,3 +1,6 @@
-class Prato:
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+class Prato(ItemCardapio):
     def __init__(self, nome, preco, descricao):
-        pass
+        super().__init__ (nome, preco)
+        self.descricao = descricao
